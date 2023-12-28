0067b856b4fe3c07ba7e76aee5cff9d9b3756fca
krimisha <krimisha025@gmail.com>
2023-12-28 17:09:43 +0000

    programm

---
+++ LEC1Q1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("Myself Krimisha Bhanderi.....\n");
+	printf("I am 18 Years old\n");
+	printf("I have studied from kaushal vidhya bhavan\n");
+	printf("currently i am studying full stack web developement course\n");
+	printf("Grid:3930\n");
+	printf("I am from surat");
+getch();
+}
